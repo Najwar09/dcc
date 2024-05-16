c1339f39572cac9032a33e7a4745c80ad9aa54cc
Najwar09 <wasih09@gmail.com>
2024-05-16 22:37:20 +0800

    Pembuatan Struktur Folder

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Router from './routes'
+
+const app = () => {
+    return (
+        <>
+            <Router />
+        </>
+    )
+}
+
+export default app
+
+const styles = StyleSheet.create({})
